9fd96c1e0d680f21d6536a65f2cbc4a6849138b2
Rishi Ravikumar <rishi.ravikumar@icloud.com>
2023-10-29 04:03:50 +0000

    Quiz navbar created

--- guh2023/lib/navbar.dart
+++ guh2023/lib/navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guh2023/homepage.dart';
 import 'quizbrain.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
@@ -10,142 +11,37 @@ class Navbar extends StatelessWidget {
     return Drawer(
         child: ListView(
       children: [
-        ListTile(title: Text('Quiz 1')),
         ListTile(
-          title: Text('Quiz 2'),
+          title: Text('Quiz 1'),
           onTap: () {
+            quizBrain.setQuizNo(1);
             Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (BuildContext context) => QuizPage()));
+                    builder: (BuildContext context) => HomePage()));
           },
         ),
-        ListTile(title: Text('Quiz 3')),
-        ListTile(title: Text('Quiz 4')),
-        ListTile(title: Text('Quiz 5'))
+        ListTile(
+          title: Text('Quiz 2'),
+          onTap: () {
+            quizBrain.setQuizNo(2);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => HomePage()));
+          },
+        ),
+        ListTile(
+          title: Text('Quiz 3'),
+          onTap: () {
+            quizBrain.setQuizNo(3);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => HomePage()));
+          },
+        ),
       ],
     ));
   }
 }
-
-class QuizPage extends StatefulWidget {
-  const QuizPage({super.key});
-
-  @override
-  State<QuizPage> createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [];
-  int score = 0;
-
-  void checkAnswer({required bool userPickedAnswer}) {
-    bool correctAnswer = quizBrain.getQuestionAnswer();
-
-    setState(() {
-      if (quizBrain.isFinished() == true) {
-        Alert(
-          context: context,
-          title: 'Finished!',
-          desc:
-              'You\'ve reached the end of the quiz, !!!!!and you have a score of ${score}.',
-        ).show();
-        quizBrain.reset();
-        scoreKeeper.clear();
-      } else {
-        if (correctAnswer == userPickedAnswer) {
-          quizBrain.nextQuestion();
-          score += 1;
-          scoreKeeper.add(const Icon(
-            Icons.check,
-            color: Colors.green,
-          ));
-        } else {
-          quizBrain.nextQuestion();
-          score -= 1;
-          scoreKeeper.add(const Icon(
-            Icons.close,
-            color: Colors.red,
-          ));
-        }
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff222222),
-      body: SafeArea(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: Center(
-                  child: Text(
-                    quizBrain.getQuestionText(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 30.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
-                  onPressed: () {
-                    // the user picked true.
-                    checkAnswer(userPickedAnswer: true);
-                  },
-                  child: const Text(
-                    'True',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
-                  onPressed: () {
-                    // the user picked false.
-                    checkAnswer(userPickedAnswer: false);
-                  },
-                  child: const Text(
-                    'False',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-            Row(children: scoreKeeper),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-QuizBrain quizBrain = QuizBrain();
--- guh2023/lib/quizbrain.dart
+++ guh2023/lib/quizbrain.dart
@@ -2,24 +2,36 @@ import 'question.dart';
 
 class QuizBrain {
   late int _questionNumber = 0;
+  int quiz_no = 1;
 
-  final List<Questions> _questionBank = [
-    Questions(
-        questionText: 'Lions are in the cat family.', questionAnswer: true),
-    Questions(questionText: '20 - 12 = 10', questionAnswer: false),
-    Questions(
-        questionText: 'A group of pigeons is called a cacophony.',
-        questionAnswer: false),
-    Questions(questionText: '33 * 3 = 99', questionAnswer: true),
-    Questions(
-        questionText: 'Buzz Aldrin was the first man to set foot on the moon.',
-        questionAnswer: false),
-    Questions(
-        questionText: 'Matter is made up of atoms.', questionAnswer: true),
+  final List<List<Questions>> _questionBank = [
+    [
+      Questions(
+          questionText: 'Lions are in the cat family.', questionAnswer: true),
+      Questions(questionText: '20 - 12 = 10', questionAnswer: false)
+    ],
+    [
+      Questions(
+          questionText: 'A group of pigeons is called a cacophony.',
+          questionAnswer: false),
+      Questions(questionText: '33 * 3 = 99', questionAnswer: true)
+    ],
+    [
+      Questions(
+          questionText:
+              'Buzz Aldrin was the first man to set foot on the moon.',
+          questionAnswer: false),
+      Questions(
+          questionText: 'Matter is made up of atoms.', questionAnswer: true)
+    ]
   ];
 
+  void setQuizNo(int quiz_no) {
+    this.quiz_no = quiz_no;
+  }
+
   void nextQuestion() {
-    if (_questionNumber < _questionBank.length - 1) {
+    if (_questionNumber < _questionBank[quiz_no - 1].length - 1) {
       _questionNumber++;
     }
   }
@@ -27,15 +39,15 @@ class QuizBrain {
   // This methods is now acts as a getter and setter (Encapsulation in action).
 
   String getQuestionText() {
-    return _questionBank[_questionNumber].questionText;
+    return _questionBank[quiz_no - 1][_questionNumber].questionText;
   }
 
   bool getQuestionAnswer() {
-    return _questionBank[_questionNumber].questionAnswer;
+    return _questionBank[quiz_no - 1][_questionNumber].questionAnswer;
   }
 
   bool isFinished() {
-    if (_questionNumber >= _questionBank.length - 1) {
+    if (_questionNumber >= _questionBank[quiz_no - 1].length - 1) {
       return true;
     } else {
       return false;
